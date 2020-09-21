a868fa296e871e4cb40573fb441b85a9f10e5469
Vadim51515 <68582499+Vadim51515@users.noreply.github.com>
2020-09-21 22:29:23 +0500

    verstka

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styles from './MyPosts.module.css';
 import Post from './Post/Post';
 const MyPosts = () => {
+    let postData = [
+        { id: 0, message: "Hello Vadim, you is beatiful", like:25 },
+        { id: 1, message: " I dont want speek with you",like:15 },
+    ]
     return (
         <div className={styles.posts}>
-           <Post message= "Hello Vadim, you is beatiful" like="15"/>
+           <Post message={postData[0].message}  like={postData[0].like}/>
            <hr />
-           <Post message=" I dont want speek with you" like="25"/>
+           <Post message={postData[1].message} like={postData[1].like}/>
            <hr />
         </div>
     )
