3c78cc45dc01f525553b9e587613f97d43d365a3
Vadim51515 <68582499+Vadim51515@users.noreply.github.com>
2021-05-17 16:32:17 +0500

    Updates

---
+++ precache-manifest.79b6f211582dc5d3ddcaa69ac271c0b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1915f5b66e3f33e151d5a9b7673bc8f",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "7b80b4a9b6bbaa95694a",
+    "url": "/my-project/static/css/3.91bf801e.chunk.css"
+  },
+  {
+    "revision": "aed5d51940b3dc353012",
+    "url": "/my-project/static/css/4.e7a5f907.chunk.css"
+  },
+  {
+    "revision": "fadf2f9ed9ac18af166f",
+    "url": "/my-project/static/css/main.ebfd3a24.chunk.css"
+  },
+  {
+    "revision": "4902e8c6f54c35a33006",
+    "url": "/my-project/static/js/2.6f70a85c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-project/static/js/2.6f70a85c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b80b4a9b6bbaa95694a",
+    "url": "/my-project/static/js/3.7a2146d0.chunk.js"
+  },
+  {
+    "revision": "aed5d51940b3dc353012",
+    "url": "/my-project/static/js/4.45a66cea.chunk.js"
+  },
+  {
+    "revision": "fadf2f9ed9ac18af166f",
+    "url": "/my-project/static/js/main.7c85cc74.chunk.js"
+  },
+  {
+    "revision": "e63af44b1a137a377e22",
+    "url": "/my-project/static/js/runtime-main.b9476714.js"
+  },
+  {
+    "revision": "5d407a8835caa07ac7446190098f800b",
+    "url": "/my-project/static/media/preloader.5d407a88.svg"
+  }
+]);
